221432ddf72a344ac453b7afe3b6317f15261741
Carter Temm <cartertemm@gmail.com>
2024-04-14 23:02:20 -0700

    Bump the last tested NVDA version

--- buildVars.py
+++ buildVars.py
@@ -42,7 +42,7 @@ Press NVDA+shift+i to pop up a menu asking how you wish to describe based on the
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2023.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2023.2",
+	"addon_lastTestedNVDAVersion": "2024.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
