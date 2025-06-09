01a56afc79bcdf944121486cf024725d04b539fe
Carter Temm <cartertemm@gmail.com>
2025-06-09 16:50:38 -0700

    Bump lastTestedNVDAVersion to 2024.4

--- buildVars.py
+++ buildVars.py
@@ -44,7 +44,7 @@ The process is similar for other model providers, see add-on documentation for m
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2023.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2024.1",
+	"addon_lastTestedNVDAVersion": "2024.4",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
